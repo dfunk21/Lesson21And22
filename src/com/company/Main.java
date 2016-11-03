84814997ccc4155bd80d49415a1e40b6eb43627b
df377 <df377@pvsd.org>
2016-11-03 14:30:26 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,30 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	//Lesson 21 and 22
+
+        //Wrapper Classes
+            //Classes that convert dtata types into objects
+            //Wrap number, boolean, or char in an object
+
+        //Four big ones
+            //Integer, double, boolean, character
+                //Integer ic = 7;
+                //int i = 10
+                //Integer ii = i; (now an object)
+
+        //Autoboxing, automatic conversion between primitive data types and corresponding classes
+            //No longer needed!
+
+        //Conversion of an int to a String
+            //public StringtoString(int i);
+
+                //int i = 104;
+                //String ss = Integer.toString(i);
+
+                //int j = 3;
+                //String s = "" + j; (s will be equal to "3")
+    }
+}
